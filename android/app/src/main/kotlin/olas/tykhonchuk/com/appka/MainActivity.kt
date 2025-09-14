cb36c44f34f308dfa60a93a5e6e35764a012ebdc
Olha Tykhonchuk <tykhonchuk.ola@gmail.com>
2025-09-14 19:37:45 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/olas/tykhonchuk/com/appka/MainActivity.kt
@@ -0,0 +1,5 @@
+package olas.tykhonchuk.com.appka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(
+        title: Text( "Appka", style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+    ),
+  ));
+
+}
